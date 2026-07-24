5923c52e9eada7ea6b5d0b2b8c5b74bf7b8f95e8
Bala25-ca <cbaladevi@gmail.com>
2026-07-23 18:12:27 -0700

    generate fibonacci numbers

--- external_package.py
+++ external_package.py
@@ -1,3 +1,7 @@
+#Note: Type in Terminal to install colorama package or requests package.
+#python -m pip install requests
+#python -m pip install colorama
+
 import requests
 
 def get_weather():
@@ -5,3 +9,4 @@ def get_weather():
     url = "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&hourly=temperature_2m"
     response = requests.get(url).json()
     return response['hourly']['temperature_2m']
+print(get_weather())

38e99bae1a0ebb76fc3b66ed31c47fd3c9774155
Bala25-ca <cbaladevi@gmail.com>
2026-07-24 23:40:46 -0700

    Basic Bank Account

---
+++ bank.py
@@ -0,0 +1,42 @@
+# Basic Bank Account Management System
+class BankAccount:
+    def __init__(self, account_number, account_owner, balance=0):
+        self.account_number = account_number
+        self.account_owner = account_owner
+        self.balance = balance
+
+    def get_account_number(self):
+        return self.account_number
+
+    def get_account_owner(self):
+        return self.account_owner
+
+    def get_balance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance+= amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient funds.")
+
+    def __str__(self):
+        return f"Account Number: {self.account_number}, Account Holder: {self.account_owner}, Balance: ${self.balance}"
+
+account_number = input("Enter account number: ")
+account_owner = input("Enter account owner name: ")
+balance = float(input("Enter initial balance: "))
+
+account1= BankAccount(account_number, account_owner, balance)
+print(account1.get_balance())
+
+amount=float(input("Enter amount to deposit: "))
+account1.deposit(amount)
+print(account1.get_balance())
+
+amount=float(input("Enter amount to withdraw: "))
+account1.withdraw(amount)
+print(account1.get_balance())
